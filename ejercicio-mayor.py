95ca5d9ead562751895650c94418eebee2b54f36
shahsawar <36155416+shahsawar@users.noreply.github.com>
2018-02-09 12:58:55 +0100

    Add files via upload

---
+++ ejercicio-mayor.py
@@ -0,0 +1,10 @@
+Numero1=input("Introduzca un valor ")
+Numero2=input("Introduzca otro valor ")
+
+if Numero1>Numero2:
+	print("Valor 2 es mayor")
+else:
+	if Numero1<Numero2:
+		print("Valor 1 es mayor")
+	else:
+		print("Son iguales")
