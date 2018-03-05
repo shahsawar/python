c18669c9d4efe2ff88562c2b855074f68b5ae12b
shahsawar <36155416+shahsawar@users.noreply.github.com>
2018-03-05 12:00:29 +0100

    Add files via upload

---
+++ Bucles.py
@@ -0,0 +1,17 @@
+""" coding:utf-8
+    shah sawar
+    5/3/2018
+"""    
+
+#Inicializaciones
+salir= "N"
+anyo=2001
+
+while (salir=="N"):
+       #Hago cosas
+       print "Informes de Años",anyo
+       #Incremento
+       anyo= anyo + 1
+       #Activo indicador de salida si toca
+       if (anyo > 2003):#Condicion salida
+           salir="S"
