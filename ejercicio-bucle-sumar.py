704926f27bee84452badcafc77e21784dc0d18b5
shahsawar <36155416+shahsawar@users.noreply.github.com>
2018-03-05 16:53:40 -0500

    Add files via upload

---
+++ ejercicio-bucle-sumar.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+#shah sawar
+#05/03/2018
+
+import os
+os.system("clear")
+
+print "\n"
+print("          Sumar desde el número 1 hasta el 5")
+print "\n"
+suma = 0
+for i in [1, 2, 3, 4, 5]:
+    suma = suma + i
+print "                  1+2+3+4+5=",suma
+print "\n"
+
