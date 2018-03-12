ab9bbd53f698e8af376430d0f02a3fc84b594a30
shahsawar <36155416+shahsawar@users.noreply.github.com>
2018-03-12 15:26:40 -0400

    Add files via upload

---
+++ Ejercicio-bucle-sumar-pares.py
@@ -0,0 +1,20 @@
+"""
+coding:utf-8
+shah sawar
+05/03/2018
+"""
+#Inicializaciones
+salir="N"
+numero=1
+
+while (salir=="N"):
+       #Hago cosas
+       if numero%2==0:
+           print numero
+       #Incremento
+       numero=numero + 1
+       #Activo indicador de salida si toca
+       if (numero>5):#Condicion salida
+		    print 2+4
+		     
+		    salir="S"
