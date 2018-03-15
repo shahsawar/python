96936b3f56b16889cdcd9784d7e4731e2d28282f
shahsawar <36155416+shahsawar@users.noreply.github.com>
2018-03-15 12:46:27 -0400

    Add files via upload

---
+++ ejercicio-bucle-sumar2.py
@@ -0,0 +1,22 @@
+"""coding:utf-8
+shah sawar
+15/03/2018
+"""
+# Inicializaciones
+salir = "N"
+numero=1
+maximo=5
+suma=0
+while ( salir=="N" ):
+    # Hago cosas
+    if (numero%2==0):
+        print (numero)
+
+        suma=suma + numero
+    # Incremento
+    numero=numero + 1
+    # Activo indicador de salida si toca
+    if (numero > maximo): # Condición de salida
+            salir = "S"
+
+print ("=",suma)
